29967b89757917cc626693778b10e817a4ca8681
Ismail Alammar <ialammar@KLNB-201.local>
2022-03-19 14:36:33 +0800

    initial app

---
+++ src/main/java/com/server/healthchecker/HealthCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.server.healthchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCheckerApplication.class, args);
+	}
+
+}
